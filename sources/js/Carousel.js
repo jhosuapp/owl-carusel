17b329bb50c15864a62b417a96b1995eba7f0536
jhosuappp <joshua@150porciento.com>
2022-07-14 20:25:58 -0500

    Se deja estructura para y clases listas para uso en proyectos

--- sources/js/Carousel.js
+++ sources/js/Carousel.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
         items = 4,
         loop = false,
         margin = 10,
-        nav = false,
+        nav = false, imgL = "../sources/img/left.svg", imgR = "../sources/img/right.svg",
         autoplay = false,
         autoplayTimeout = 0,
         dots = false,
@@ -38,33 +38,58 @@ window.addEventListener('DOMContentLoaded', ()=>{
             }
         });
 
-        //CREATE CLASES DOM
+        //CREAR NAV Y DOTS
+        var objectToArrray = Object.values(clase);
 
-        if(dots == true){
-            console.log('hola');
+        if(nav){
+            objectToArrray[0].classList.add('owl-carousel__nav-active');
+
+            let getOwlNav = document.querySelectorAll('.owl-carousel__nav-active .owl-nav button');
+
+            let createImageLeft = document.createElement('img');
+            let createImageRight = document.createElement('img');
+
+            createImageLeft.classList.add('owl-carousel__nav-active__left');
+            createImageRight.classList.add('owl-carousel__nav-active__right');
+
+            createImageLeft.setAttribute('src', imgL);
+            createImageRight.setAttribute('src', imgR);
+
+            getOwlNav[0].append(createImageLeft);
+            getOwlNav[1].append(createImageRight);
         }
 
-        if(nav == true){
-            console.log('hola');
-        }
+        
+        dots == true ? objectToArrray[0].classList.add('owl-carousel__dots-active') : ()=>{return false};
+
     }
 
     var getOwlCarouselTwoitems = $(".owl-carousel.owl-carousel__two-items");
-    var object = Object.values(getOwlCarouselTwoitems)
-
-    object.forEach((data, indice)=>{
-        if(indice == 0){
-            data.classList.add('active');
-        }
-    });
-
-    carouselReuse(getOwlCarouselTwoitems, undefined, true);
+    carouselReuse(
+        getOwlCarouselTwoitems, 
+        undefined,
+        false,
+        false,
+        true,
+        undefined,
+        undefined,
+        false,
+        0,
+        true,
+        1,
+        2,
+        2,
+        2,
+    );
 
 
     var getOwlCarouselItem = $(".owl-carousel");
-    carouselReuse(getOwlCarouselItem, undefined, false);
-
-
+    carouselReuse(getOwlCarouselItem,
+        undefined,
+        false,
+        false,
+        false
+    );
 
 
 });
